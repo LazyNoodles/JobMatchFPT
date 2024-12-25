9bd11f63653a5998c314f7656aef1a1c4d3d7418
LazyNoodles <destroyer99988@gmail.com>
2024-12-25 18:17:17 +0700

    Uploaded the code into repo

---
+++ ApplicationDevelopment.WebMVC/Data/Entities/JobType.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ApplicationDevelopment.WebMVC.Data.Entities
+{
+    /// <summary>
+    /// Job Type: FullTime, Part-time, Casual, Season, Mannual, Office
+    /// Is the parent of Job (similar to Category and Product)
+    /// </summary>
+    public class JobType
+    {
+        public JobType()
+        {
+            Id = Guid.NewGuid();
+            Jobs = new Collection<Job>();
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int Position { get; set; }
+
+        public virtual ICollection<Job> Jobs { get; set; }
+    }
+}
